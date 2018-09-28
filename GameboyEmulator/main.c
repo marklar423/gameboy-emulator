eb6a598ad489a2402cc00af5ada3f9a54e382961
Mark <1843487+marklar423@users.noreply.github.com>
2018-09-28 11:32:46 -0400

    Fixed gray colors

--- GameboyEmulator/main.c
+++ GameboyEmulator/main.c
@@ -35,10 +35,10 @@ void drawFrame() {
 				red = green = blue = 1.0f;
 				break;
 			case PixelColor_LightGray:
-				red = green = blue = 0.166f;
+				red = green = blue = 0.75f;
 				break;
 			case PixelColor_DarkGray:
-				red = green = blue = 0.77f;
+				red = green = blue = 0.50f;
 				break;
 			case PixelColor_Black:
 				red = green = blue = 0.0f;
