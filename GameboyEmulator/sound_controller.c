68d39c4d304d2f730af86c199a9a3f24d118b0da
Mark <1843487+marklar423@users.noreply.github.com>
2018-10-21 15:33:52 -0400

    Tweaked channel 3 frequency converter

--- GameboyEmulator/sound_controller.c
+++ GameboyEmulator/sound_controller.c
@@ -2,7 +2,7 @@
 #include "sound_controller.h"
 #include "util.h"
 
-static float pitch = 50.0f;
+/*static float pitch = 50.0f;
 static float start_pitch = 50.0f;
 static float end_pitch = 500.0f;
 static float sweep_seconds = 3.0f;
@@ -11,15 +11,51 @@ static bool reverse_sweep = false;
 static float total_inc, inc_per_second, inc_per_frame;
 static float seconds_per_frame;
 
-static int wave_frame = 0;
+static int wave_frame = 0;*/
 
 void initSound() {
-	seconds_per_frame = 1.0f / AUDIO_SAMPLE_RATE;
+	/*seconds_per_frame = 1.0f / AUDIO_SAMPLE_RATE;
 	total_inc = end_pitch - start_pitch;
 	inc_per_second = total_inc / sweep_seconds;
-	inc_per_frame = inc_per_second * seconds_per_frame;
+	inc_per_frame = inc_per_second * seconds_per_frame;*/
 }
 
+float getChannel3Sample(SoundData *soundData) {
+	float sample = 0.0f;
+
+	if (soundData->soundOnOff && soundData->chan3_OnOff) {
+		float frequencyRaw = joinBytes(soundData->chan3_FrequencyLow, soundData->chan3_FrequencyHighSettings & 0x7);
+		float frequency = 131072.0f / (2048.0f - frequencyRaw);
+		
+		float sampleRate = AUDIO_SAMPLE_RATE;
+		float framesPerWave = sampleRate / frequency;
+		int framesPerWaveSample = framesPerWave / (SOUND_WAVE_PATTERN_SIZE * 2);
+
+		float currentTick = soundData->chan3_currentTick;
+		float sampleIndexRaw = (currentTick / framesPerWave) * (SOUND_WAVE_PATTERN_SIZE * 2);
+		int sampleIndex = (int)sampleIndexRaw % (SOUND_WAVE_PATTERN_SIZE * 2);
+
+		unsigned char sampleCombined = soundData->chan3_WaveData[sampleIndex / 2];
+		unsigned char sample4Bit;
+
+		bool useHighBits = (sampleIndex % 2 == 0);
+		if (useHighBits) sample4Bit = (sampleCombined & 0xF0) >> 4;
+		else sample4Bit = sampleCombined & 0x0F;
+
+		sample = sample4Bit / 15.0f;
+
+		SoundFlag volume = soundData->chan3_Volume & SOUND_FLAG_CHAN3_VOLUME_MASK;
+		if (volume == SOUND_FLAG_CHAN3_VOLUME_HALF) sample *= 0.5;
+		else if (volume == SOUND_FLAG_CHAN3_VOLUME_QUARTER) sample *= 0.25;
+
+		soundData->chan3_currentTick++;
+	}
+	else {
+		soundData->chan3_currentTick = 1;
+	}
+
+	return sample;
+}
 
 float tickSound(Hardware *hardware) {
 	float sample = 0.0f;
@@ -52,36 +88,7 @@ float tickSound(Hardware *hardware) {
 
 	SoundData *soundData = hardware->soundData;
 
-	if (soundData->soundOnOff && soundData->chan3_OnOff) {
-		float frequencyRaw = joinBytes(soundData->chan3_FrequencyLow, soundData->chan3_FrequencyHighSettings & 0x7);
-		float frequency = 65536.0f / (2048.0f - frequencyRaw);
-
-		float float_sample_rate = AUDIO_SAMPLE_RATE;
-		float frames_per_cycle = float_sample_rate / frequency;
-		int frames_per_sample = frames_per_cycle / (SOUND_WAVE_PATTERN_SIZE * 2);
-		
-		float tick_float = soundData->chan3_currentTick;
-		float sampleIndexRaw = (tick_float / frames_per_cycle) * (SOUND_WAVE_PATTERN_SIZE * 2);
-		int sampleIndex = (int)sampleIndexRaw % (SOUND_WAVE_PATTERN_SIZE * 2);
-
-		unsigned char sampleCombined = soundData->chan3_WaveData[sampleIndex / 2];		
-		unsigned char sample4Bit;
-		
-		bool useHighBits =  (sampleIndex % 2 == 0);
-		if (useHighBits) sample4Bit = (sampleCombined & 0xF0) >> 4;
-		else sample4Bit = sampleCombined & 0x0F;
-		
-		sample = sample4Bit / 15.0f;
-
-		SoundFlag volume = soundData->chan3_Volume & SOUND_FLAG_CHAN3_VOLUME_MASK;
-		if (volume == SOUND_FLAG_CHAN3_VOLUME_HALF) sample *= 0.5;
-		else if (volume == SOUND_FLAG_CHAN3_VOLUME_QUARTER) sample *= 0.25;
-
-		soundData->chan3_currentTick++;
-	}
-	else {
-		soundData->chan3_currentTick = 1;
-	}
+	sample = getChannel3Sample(soundData);
 
 	return sample;
 }
